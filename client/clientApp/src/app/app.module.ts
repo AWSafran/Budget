97c59171c82ce05c99e0330fd539b6f942b4d89c
Allyson O'Connell <allysono@Allysons-MacBook-Pro.local>
2022-01-24 21:54:31 -0500

    restructure page files

--- client/clientApp/src/app/app.module.ts
+++ client/clientApp/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DashboardComponent } from './pages/dashboard/dashboard/dashboard.component';
-import { ExpensesComponent } from './pages/expenses/expenses/expenses.component';
-import { ManageComponent } from './pages/manage/manage/manage.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ExpensesComponent } from './pages/expenses/expenses.component';
+import { ManageComponent } from './pages/manage/manage.component';
 import { MatTabsModule } from '@angular/material/tabs';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,9 @@ import { MatTabsModule } from '@angular/material/tabs';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatTabsModule
+    MatTabsModule,
+    ReactiveFormsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
